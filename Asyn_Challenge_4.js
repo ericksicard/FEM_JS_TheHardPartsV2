21b8b7a57ac0d19e3ae3385654fa593315d96b1a
ericksicard <esrfree@gmail.com>
2020-10-25 13:59:21 -0400

    Async Challenge 4

---
+++ Asyn_Challenge_4.js
@@ -0,0 +1,10 @@
+/*
+Create a function brokenRecord that console logs hi again every second.
+Use the End Code button to stop the console logs when you are satisfied that it is working.
+*/
+
+function brokenRecord() {
+  setInterval( () => console.log('Hi again'), 1000 )
+}
+// Uncomment the following line to check your work!
+ brokenRecord(); // should log (every second): hi again
